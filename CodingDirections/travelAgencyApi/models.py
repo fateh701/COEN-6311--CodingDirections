d497c26fe06ce76432f41d270f1a961790932a70
fateh701 <ehteshaam.09@gmail.com>
2024-02-13 18:20:37 -0500

    updat trial

--- CodingDirections/travelAgencyApi/models.py
+++ CodingDirections/travelAgencyApi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=150)
+    release_date = models.IntegerField(default=2000)
+    image = models.URLField(help_text="From imdb")
+    summary = models.TextField(help_text="short description")
