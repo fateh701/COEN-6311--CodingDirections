f577c4a274c3633cd57a32131746b67661963885
Syed Fauzan <fauzansyed.28@gmail.com>
2024-02-15 13:06:18 -0500

    Flight, Hotel, Activites and Travel Packages were added. Travel Package functionality yet to be done. - fauzan

--- CodingDirections/travelAgencyApi/views.py
+++ CodingDirections/travelAgencyApi/views.py
@@ -3,21 +3,21 @@ from .models import Flight, Hotel, Activity, TravelPackage
 from .serializers import FlightSerializer, HotelSerializer, ActivitySerializer, TravelPackageSerializer
 
 
-class FlightViewSet(viewsets.ModelViewSet):
+class FlightViewSet(viewsets.HyperlinkedModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
 
 
-class HotelViewSet(viewsets.ModelViewSet):
+class HotelViewSet(viewsets.HyperlinkedModelViewSet):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
 
 
-class ActivityViewSet(viewsets.ModelViewSet):
+class ActivityViewSet(viewsets.HyperlinkedModelViewSet):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
 
 
-class TravelPackageViewSet(viewsets.ModelViewSet):
+class TravelPackageViewSet(viewsets.HyperlinkedModelViewSet):
     queryset = TravelPackage.objects.all()
     serializer_class = TravelPackageSerializer
