6d2daa5ccc5124211b492a41136e21ff755f0f77
Syed Fauzan <fauzansyed.28@gmail.com>
2024-02-15 13:04:21 -0500

    Your commit message here

--- CodingDirections/travelAgencyApi/models.py
+++ CodingDirections/travelAgencyApi/models.py
@@ -1,7 +1,41 @@
 from django.db import models
 
-class Movie(models.Model):
-    title = models.CharField(max_length=150)
-    release_date = models.IntegerField(default=2000)
-    image = models.URLField(help_text="From imdb")
-    summary = models.TextField(help_text="short description")
+
+class Flight(models.Model):
+    name = models.CharField(max_length=100)
+    departure_city = models.CharField(max_length=100)
+    arrival_city = models.CharField(max_length=100)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class TravelPackage(models.Model):
+    name = models.CharField(max_length=100)
+    flights = models.ManyToManyField(Flight)
+    hotels = models.ManyToManyField(Hotel)
+    activities = models.ManyToManyField(Activity, blank=True)
+
+    def __str__(self):
+        return self.name
