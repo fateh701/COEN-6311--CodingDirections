84f639aa8eeaf2df446b3f142cb12bd72498e040
Prinkaldeep Kaur <prinkaldeep.kaur@mail.concordia.ca>
2024-02-19 01:07:58 -0500

    modification

--- CodingDirections/travelAgencyApi/serializers.py
+++ CodingDirections/travelAgencyApi/serializers.py
@@ -1,24 +1,19 @@
 from rest_framework import serializers
 from .models import Flight, Hotel, Activity, TravelPackage, Modification
-
-
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
-
 
 class HotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hotel
         fields = '__all__'
 
-
 class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
         fields = '__all__'
-
 
 class TravelPackageSerializer(serializers.ModelSerializer):
     flights = FlightSerializer(many=True)
@@ -28,6 +23,7 @@ class TravelPackageSerializer(serializers.ModelSerializer):
     class Meta:
         model = TravelPackage
         fields = '__all__'
+
 class ModificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Modification
