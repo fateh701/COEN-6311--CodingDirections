7834b9b5b2f1c956b569b2e5a6703c50bd37a466
puji4ml <pujanpravinbhai.patel@mail.concordia.ca>
2024-02-21 00:28:41 -0500

    Added each travel packages components hyperlink -Patch by Pujan 21-2-2024

--- CodingDirections/frontend/src/app/activities/activities.component.ts
+++ CodingDirections/frontend/src/app/activities/activities.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SharedService} from "../shared.service";
 
 @Component({
   selector: 'app-activities',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './activities.component.css'
 })
 export class ActivitiesComponent {
+  activitiesList: any = [];
+
+  constructor(private service: SharedService) {
+    this.getActivitiesList();
+  }
+
+  // Get all flights list,incase in future if we need so.. PAtch by Pujan 20/2/24
+  getActivitiesList = () => {
+    this.service.getActivitiesList().subscribe(
+      data => {
+        this.activitiesList = data;
+      },
+      error => {
+        console.log(error);
+      },
+    );
+  }
 
 }
