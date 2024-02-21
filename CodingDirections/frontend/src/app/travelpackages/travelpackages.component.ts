10fdfca8b1033ba4980aafb0cd77b16df8b80a65
puji4ml <pujanpravinbhai.patel@mail.concordia.ca>
2024-02-20 22:52:05 -0500

    Added Search bar for filtering travel packages in frontend -Patch by Pujan 20-2-2024

--- CodingDirections/frontend/src/app/travelpackages/travelpackages.component.ts
+++ CodingDirections/frontend/src/app/travelpackages/travelpackages.component.ts
@@ -8,6 +8,9 @@ import {SharedService} from "../shared.service";
 })
 export class TravelpackagesComponent {
   travelPackagesList: any = [];
+  filteredTravelPackagesList: any = [];  // This is the list that will be used to store the searched query travel packages
+  searchQuery: string = '';  // This is the variable that will be used to store the search query
+
 
   constructor(private service: SharedService) {
     this.getTravelpackagesList();
@@ -17,10 +20,22 @@ export class TravelpackagesComponent {
     this.service.getTravelpackagesList().subscribe(
       data => {
         this.travelPackagesList = data;
+        this.filteredTravelPackagesList = data;  // Initially the filtered list will be the same as the original list
       },
       error => {
         console.log(error);
       },
     );
   }
+
+  // This function will be used to filter the travel packages list based on the search query entered by the user -Patch by Pujan 20/2/2024
+  getFilteredTravelpackagesList() {
+    if (this.searchQuery.trim()==='') {
+      this.filteredTravelPackagesList = this.travelPackagesList;  // If the search query is empty, then the filtered list will be the same as the original list
+    } else {
+      this.filteredTravelPackagesList = this.travelPackagesList.filter((packageItem: any) =>
+          packageItem.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+    }
 }
