fcf07f0a9ca933b5b81c39bb0106f22015b75d2c
Prinkaldeep Kaur <prinkaldeep.kaur@mail.concordia.ca>
2024-03-27 02:14:46 -0400

    Delete/ modify

--- CodingDirections/frontend/src/app/bookings/bookings-all/bookings-all.component.ts
+++ CodingDirections/frontend/src/app/bookings/bookings-all/bookings-all.component.ts
@@ -50,15 +50,13 @@ export class BookingsAllComponent {
   }
 
 }
-editBooking(booking: any) {
-  // You can implement edit functionality here, such as routing to an edit page.
+  editBooking(booking: any) {
   console.log('Editing booking:', booking);
   }
   deleteBooking(bookingId: number) {
     if (confirm('Are you sure you want to delete this booking?')) {
       this.service.deleteBooking(bookingId).subscribe(
         () => {
-          // Remove the deleted booking from the list
           this.allbookingsbyID = this.allbookingsbyID.filter(booking => booking.id !== bookingId);
           console.log('Booking deleted successfully');
         },
